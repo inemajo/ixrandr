117b17164b943c37b45c08d172bf4399c5921aa6
inemajo <inemajo@fsfe.org>
2014-10-26 14:44:56 +0100

    ixrandr

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 import os
 from setuptools import setup, find_packages
 
+extras = []
+if os.path.exists('/etc/bash_completion.d'):
+    extras += [('/etc/bash_completion.d', ['extra/bash-completion/ixrandr'])]
+elif os.path.exists('/usr/share/bash-completion/'):
+    extras += [('/usr/share/bash-completion/', ['extra/bash-completion/ixrandr'])]
+
+if os.path.exists('/usr/share/zsh/site-functions/'):
+    extras += [('/usr/share/zsh/site-functions/', ['extra/zsh-completion/_ixrandr'])]
+print(extras)    
 setup(
     name="ixrandr",
-    version="0.1",
+    version="0.2",
     author="Inemajo",
     author_email="inemajo@fsfe.org",
     description="interactive xrandr",
@@ -13,6 +22,8 @@ setup(
         'Environment :: Console',
         "Natural Language :: English",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
     ],
     scripts = ['ixrandr'],
+    data_files=extras
 )
