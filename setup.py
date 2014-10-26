a37e70c0dcd1d7eef5cd1ae404cc04216a3105a0
inemajo <inemajo@fsfe.org>
2014-10-26 12:43:41 +0100

    ixrandr

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 import os
 from setuptools import setup, find_packages
 
-src_directory = 'ixrandr'
 setup(
     name="ixrandr",
     version="0.1",
     author="Inemajo",
     author_email="inemajo@fsfe.org",
     description="interactive xrandr",
+    long_description=open('README.md').read(),
     classifiers=[
         'Programming Language :: Python',
+        'Environment :: Console',
         "Natural Language :: English",
         "Programming Language :: Python :: 2.7",
     ]
+    scripts=["ixrandr"]
 )
