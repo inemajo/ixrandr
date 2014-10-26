8b7f9d0d0f551d4cce0b79bd674ab7a0db347551
inemajo <inemajo@fsfe.org>
2014-10-26 12:35:38 +0100

    ixrandr

---
+++ setup.py
@@ -0,0 +1,16 @@
+import os
+from setuptools import setup, find_packages
+
+src_directory = 'ixrandr'
+setup(
+    name="ixrandr",
+    version="0.1",
+    author="Inemajo",
+    author_email="inemajo@fsfe.org",
+    description="interactive xrandr",
+    classifiers=[
+        'Programming Language :: Python',
+        "Natural Language :: English",
+        "Programming Language :: Python :: 2.7",
+    ]
+)
